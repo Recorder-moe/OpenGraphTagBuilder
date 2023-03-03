ebab5d850e210c013c3efacf82727d9d52e35530
陳鈞 <jim60105@gmail.com>
2023-03-03 23:00:13 +0800

    跳過不該加og tag的頁面

--- src/handler.ts
+++ src/handler.ts
@@ -1,32 +1,33 @@
 export async function handleRequest(request: Request): Promise<Response> {
   const response = await fetch(request)
-
-  if (!response.ok) {
-    return response
-  }
-
+  const responseClone = response.clone()
   const headers = new Headers(response.headers)
 
-  // 检查内容类型是否为HTML
-  if (!headers.get('content-type')!.includes('text/html')) {
-    return response
+  if (
+    response.url.includes('/api/') ||
+    request.method !== 'GET' ||
+    !response.ok ||
+    !headers.get('content-type')?.includes('text/html')
+  ) {
+    return responseClone
   }
 
   const html = await response.text()
-  // 此处可以通过解析 HTML 来获取对应的 meta 标签内容
+  if (!html || !html.includes('<head>')) {
+    return responseClone
+  }
+
   const title = 'Recorder.moe'
   const description =
     'Recorder.moe is a live recording service project, the main recording content is Vtuber live video. This project is centered on "video recording service".'
 
-  // 在<head>标记中插入Meta标签
   const metaTags = `
-<meta property="og:title" content="${title}">
-<meta property="og:description" content="${description}">
-<meta name="twitter:title" content="${title}">
-<meta name="twitter:description" content="${description}">
-`
+    <meta property=\"og:title\" content=\"${title}\">
+    <meta property=\"og:description\" content=\"${description}\">
+    <meta name=\"twitter:title\" content=\"${title}\">
+    <meta name=\"twitter:description\" content=\"${description}\">
+    `
 
-  // 将 meta 标签插入到页面头部
   const newHtml = html.replace('<head>', `<head>${metaTags}`)
 
   return new Response(newHtml, {
