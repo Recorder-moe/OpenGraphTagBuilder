172b413fdae8f1bc8227abf403e189f7bd2c372f
陳鈞 <jim60105@gmail.com>
2023-03-14 21:18:33 +0800

    調整未錄影的檔案的預覧顯示，重構程式碼

--- src/Services/PrepareDataService.ts
+++ src/Services/PrepareDataService.ts
@@ -15,20 +15,36 @@ export class PrepareDataService {
   async PrepareVideoMetadata(videoId: string, channelId: string): Promise<IMetaData> {
     const video = await this.publicCosmosdbService.getVideoById(videoId, channelId);
     const channel = await this.publicCosmosdbService.getChannelById(channelId);
-    let Description = `Hey, take a look at this awesome video archive on Recorder.moe!`;
-    if (
-      (video.SourceStatus ?? 0) >= VideoStatus.Expired &&
-      video.SourceStatus !== VideoStatus.Exist &&
-      video.Status === VideoStatus.Archived
-    ) {
-      Description = "We've got your back! The video may be gone, but we've archived it for you.";
-    }
-    if (video.Status === VideoStatus.Expired) {
-      Description =
-        'Oops, this video is no longer available as it was archived on Recorder.moe more than 30 days ago.';
-    }
-    if (video.Status >= VideoStatus.Expired && video.SourceStatus !== VideoStatus.Exist) {
-      Description = "Sorry, this video can't be found on Recorder.moe.";
+
+    const { SourceStatus, Status } = video;
+    const sourceNotExist =
+      (SourceStatus ?? 0) >= VideoStatus.Expired && SourceStatus !== VideoStatus.Exist;
+    const archived = Status >= VideoStatus.Archived && Status < VideoStatus.Expired;
+    const scheduled = Status >= VideoStatus.Scheduled && Status < VideoStatus.Archived;
+    const expired = Status === VideoStatus.Expired;
+    const notArchived = !archived && !scheduled;
+
+    const messages = {
+      default: 'Hey, take a look at this awesome video archive on Recorder.moe!😎',
+      archivedAndSourceNotExist:
+        "We've got your back! The video may be gone, but we've archived it for you.💪",
+      expired:
+        'Oops, this video is no longer available as it was archived on Recorder.moe more than 30 days ago.😕',
+      notExist: "Sorry, this video can't be found on Recorder.moe.😥",
+      scheduled:
+        "This video isn't available in our archive just yet, but don't worry - we're keeping an eye on it for you!👀",
+    };
+
+    let Description = messages.default;
+
+    if (archived && sourceNotExist) {
+      Description = messages.archivedAndSourceNotExist;
+    } else if (expired) {
+      Description = messages.expired;
+    } else if (sourceNotExist || notArchived) {
+      Description = messages.notExist;
+    } else if (scheduled) {
+      Description = messages.scheduled;
     }
 
     return {
@@ -40,10 +56,14 @@ export class PrepareDataService {
 
   async PrepareChannelMetadata(channelId: string): Promise<IMetaData> {
     const channel = await this.publicCosmosdbService.getChannelById(channelId);
-    let Description = `Recorder.moe is keeping an eye on ${channel.ChannelName}'s livestream! We're all about helping you capture those important moments in real time.`;
-    if (!channel.Monitoring) {
-      Description = `Hey there! Recorder.moe is not monitoring ${channel.ChannelName} anymore. We need your help to keep the service up and running!`;
-    }
+    const { ChannelName, Monitoring } = channel;
+
+    const messages = {
+      monitoring: `Recorder.moe is keeping an eye on ${ChannelName}'s livestream! We're all about helping you capture those important moments in real time.👀`,
+      notMonitoring: `Hey there! Recorder.moe is not monitoring ${ChannelName} anymore. We need your help to keep the service up and running!🙏`,
+    };
+
+    const Description = Monitoring ? messages.monitoring : messages.notMonitoring;
 
     return {
       Title: `Recorder.moe | ${channel.ChannelName}@${channel.Source}`,
